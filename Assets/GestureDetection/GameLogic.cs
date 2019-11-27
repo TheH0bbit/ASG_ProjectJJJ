8373237f751ab3164c7ffecdb929c5ccce9c5047
RPenz <raphaelpenz135@gmail.com>
2019-11-27 14:52:34 +0100

    Upload Version 1.0

---
+++ Assets/GestureDetection/GameLogic.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+    public GestureController gc;
+
+	// Use this for initialization
+	void Start () {
+        gc.GestureRecognizedInController += OnGestureRecognized;
+        IRelativeGestureSegment[] swipeLeft = { new SwipeToLeftSegment1(), new SwipeToLeftSegment2(), new SwipeToLeftSegment3() };
+        gc.AddGesture("SwipeLeft", swipeLeft);
+        IRelativeGestureSegment[] waveLeft = { new WaveLeftSegment1(), new WaveLeftSegment2(),
+                                             new WaveLeftSegment1(),new WaveLeftSegment2(),
+                                             new WaveLeftSegment1(),new WaveLeftSegment2() };
+        gc.AddGesture("WaveLeft", waveLeft);
+
+        IRelativeGestureSegment[] pullLeft = { new PullToLeftSegment1(), new PullToLeftSegment2(), new PullToLeftSegment3(), };
+        gc.AddGesture("PullLeft", pullLeft);
+	}
+
+    void OnGestureRecognized(object sender, GestureEventArgs e)
+    {
+        if (e.GestureName == "SwipeLeft")
+        {
+            Debug.Log("Swipe Recognized");
+
+        }
+        if (e.GestureName == "WaveLeft")
+        {
+            Debug.Log("Wave Recognized");
+        }
+        if (e.GestureName == "PullLeft")
+        {
+            Debug.Log("PullLeft Recognized");
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
