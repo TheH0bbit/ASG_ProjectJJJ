7b1c3c028c12fc9d2f41961911d0dcc4580b785c
JoshuaWeggartner <Joshua.Weggartner@gmx.de>
2019-12-03 15:04:01 +0100

    trackgen

---
+++ Assets/TrackGen/Kinect/Generator.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+
+    public GameObject[] StreckenStücke;
+    public Transform Strecke;
+
+    public float speed;
+    public float rotspeed;
+    public float heightspeed;
+
+    public int vorlauf;
+
+    public Transform nextSpawnpoint;
+    // Use this for initialization
+    void Start () {
+        nextSpawnpoint = this.transform;
+        int x = Random.Range(0, StreckenStücke.Length);
+        for (int i = 0; i < vorlauf; i++)
+        {
+          GameObject g=  Instantiate(StreckenStücke[x], nextSpawnpoint.position, Quaternion.identity, Strecke);
+            nextSpawnpoint = g.transform.GetChild(0).GetComponent<Streckenstück>().mySpawnpoint;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Strecke.Translate(Vector3.back * Time.deltaTime * speed);
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Rotate(rotspeed,1);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Rotate(rotspeed, -1);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            ChangeHeight(heightspeed, -1);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            ChangeHeight(heightspeed, 1);
+        }
+
+    }
+
+    public void SpawnNext()
+    {
+        int x = Random.Range(0, StreckenStücke.Length);
+        GameObject g = Instantiate(StreckenStücke[x], nextSpawnpoint.position, Quaternion.identity, Strecke);
+        nextSpawnpoint = g.transform.GetChild(0).GetComponent<Streckenstück>().mySpawnpoint;
+
+
+
+    }
+
+
+    public void Rotate(float speed, int dir)
+    {
+
+        if (dir > 0)
+        {
+            Strecke.transform.Rotate(Vector3.back * Time.deltaTime * speed);
+        }
+        else
+        {
+            Strecke.transform.Rotate(Vector3.forward*Time.deltaTime*speed);
+        }
+
+
+    }
+
+   public void ChangeHeight(float speed, int dir)
+    {
+
+        if (dir > 0)
+        {
+            Strecke.transform.Translate(Vector3.up * Time.deltaTime * speed, Space.World);
+        }
+        else
+        {
+            Strecke.transform.Translate(Vector3.down * Time.deltaTime * speed,Space.World);
+        }
+
+    }
+
+}
---
+++ Assets/TrackGen/Kinect/Streckenstück.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Streckenstück : MonoBehaviour {
+
+   
+    public Transform mySpawnpoint;
+    public Generator gen;
+    public float speed;
+
+    public GameObject[] obstacles;
+
+    public Transform[] oSpawnpoints;
+
+	// Use this for initialization
+	void Start () {
+        gen = GameObject.FindObjectOfType<Generator>();
+      // gen.nextSpawnpoint = mySpawnpoint;
+
+
+        foreach(Transform t in oSpawnpoints)
+        {
+            int x = Random.Range(0, obstacles.Length );
+            GameObject g = Instantiate(obstacles[x], t.position, t.rotation, this.transform.parent.transform);
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // this.transform.parent.Translate(Vector3.back * Time.deltaTime * speed);
+    }
+
+    public void DestroyThis()
+    {
+        gen.SpawnNext();
+        Destroy(this.transform.parent.gameObject);
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Deathplane") {
+
+            Debug.Log("Trigger");
+            DestroyThis();
+                }
+    }
+
+}
